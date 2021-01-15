a99382e6a408c13c3b2e3217063990f971ddb315
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2021-01-16 00:30:48 +0900

    display all operationLogs

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Events from './Events';
 import EventForm from './EventForm';
+import OperationLogs from './OperationLogs';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
 
@@ -14,6 +15,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
