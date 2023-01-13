b932aa13c33a621f5544fed0e6dfe913585a3e10
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2023-01-13 18:16:03 +0900

    delete all operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -4,7 +4,7 @@ import {
   CREATE_EVENT,
   DELETE_ALL_EVENTS,
   ADD_OPERATION_LOG,
-  DELETE_OPERATION_LOGS,
+  DELETE_ALL_OPERATION_LOGS,
 } from '../actions';
 
 import AppContext from '../contexts/AppContext';
@@ -48,6 +48,18 @@ const EventForm = () => {
     }
   };
 
+  const deleteAllOperationLogs = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      '全ての操作ログを本当に削除しても良いですか？'
+    );
+    if (result) {
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS,
+      });
+    }
+  };
+
   const unCreatable = title === '' || body === '';
   return (
     <>
@@ -84,6 +96,13 @@ const EventForm = () => {
           disabled={state.events.length === 0}
         >
           全てのイベントを削除する
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllOperationLogs}
+          disabled={state.operationLogs.length === 0}
+        >
+          全ての操作ログを削除する
         </button>
       </form>
     </>
