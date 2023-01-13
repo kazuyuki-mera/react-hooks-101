5e5d87fdfc7d211db3041065ca34f975ed7bb703
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2023-01-13 18:00:44 +0900

    create operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,8 +1,14 @@
 import React, { useState, useContext } from 'react';
 
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS,
+} from '../actions';
 
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -16,6 +22,11 @@ const EventForm = () => {
       title,
       body,
     });
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601(),
+    });
     setTitle('');
     setBody('');
   };
@@ -28,6 +39,11 @@ const EventForm = () => {
     if (result) {
       dispatch({
         type: DELETE_ALL_EVENTS,
+      });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601(),
       });
     }
   };
